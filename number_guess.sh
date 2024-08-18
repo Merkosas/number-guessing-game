b889ceea72eb45e8d4a25202cc487633d648f309
Marcos Kees <merkosas@protonmail.com>
2024-08-18 18:30:28 +0000

    feat: Add username prompt and database check

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,21 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 # Generate random number
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+# Prompt for username
+echo "Enter your username:"
+read USERNAME
+
+# Check if user exists
+USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_INFO ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+else
+  echo "$USER_INFO" | while IFS='|' read GAMES_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+fi
