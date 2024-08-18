be03ea44c99b281fd994f47a8106b3c8054849ec
Marcos Kees <merkosas@protonmail.com>
2024-08-18 18:31:15 +0000

    feat: Implement main game logic

--- number_guess.sh
+++ number_guess.sh
@@ -22,3 +22,28 @@ else
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   done
 fi
+
+echo "Guess the secret number between 1 and 1000:"
+
+GUESS_COUNT=0
+
+while true
+do
+  read GUESS
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  else
+    GUESS_COUNT=$((GUESS_COUNT + 1))
+    if [[ $GUESS -eq $SECRET_NUMBER ]]
+    then
+      echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+      break
+    elif [[ $GUESS -gt $SECRET_NUMBER ]]
+    then
+      echo "It's lower than that, guess again:"
+    else
+      echo "It's higher than that, guess again:"
+    fi
+  fi
+done
